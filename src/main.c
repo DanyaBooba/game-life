4b233582c66d47295e2ce4c898e7e5968acef154
Daniil Dybka <danil.dybko@gmail.com>
2024-08-21 19:32:05 +0300

    Обновляет проект

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 #define POINT_LIVE 1
 #define POINT_UNLIVE 0
 
-#define POINT_LIVE_PRINT "x"
+#define POINT_LIVE_PRINT "x" // \u25A0
 #define POINT_UNLIVE_PRINT ""
 
 #define ChanceGeneratingPointAtRandom 80
@@ -201,3 +201,210 @@ void SetFieldRandomPoints() {
 		}
 	}
 }
+
+/*
+
+gen[15][4]  = 1;
+  gen[15][5]  = 1;
+  gen[15][6]  = 1;
+  gen[16][5]  = 1;
+  gen[17][5]  = 1;
+  gen[17][6]  = 1;
+  gen[17][7]  = 1;
+  gen[18][9]  = 1;
+  gen[18][10] = 1;
+  gen[18][8]  = 1;
+
+*/
+
+
+/*
+
+void rules(int array[25][100], char choice) {
+    int temp[25][100];
+    copy(array, temp);
+    for(int j = 0; j < 25; j++) {
+        for(int i = 0; i < 100; i++) {
+            int count = 0;
+            if (j>0 && j<24 && i>0 && i<99) {
+                count = array[j-1][i] +
+                        array[j-1][i-1] +
+                        array[j][i-1] +
+                        array[j+1][i-1] +
+                        array[j+1][i] +
+                        array[j+1][i+1] +
+                        array[j][i+1] +
+                        array[j-1][i+1];
+                if(count < 2 || count > 3)
+                    temp[j][i] = 0;
+                if(count == 2)
+                    temp[j][i] = array[j][i];
+                if(count == 3)
+                    temp[j][i] = 1;
+            } else {
+                if (j>0 && j<24 && i == 0) {
+                    count = array[j][99] +
+                            array[j+1][99] +
+                            array[j-1][99] +
+                            array[j][i+1] +
+                            array[j+1][i+1] +
+                            array[j-1][i+1] +
+                            array[j-1][i] +
+                            array[j+1][i];
+                    if(count < 2 || count > 3)
+                        temp[j][i] = 0;
+                    if(count == 2)
+                        temp[j][i] = array[j][i];
+                    if(count == 3)
+                        temp[j][i] = 1;
+                }
+                if (j == 0 && i>0 && i<99) {
+                    count = array[24][i+1] +
+                            array[24][i] +
+                            array[24][i-1] +
+                            array[j][i+1] +
+                            array[j+1][i+1] +
+                            array[j+1][i-1] +
+                            array[j][i-1] +
+                            array[j+1][i];
+                    if(count < 2 || count > 3)
+                        temp[j][i] = 0;
+                    if(count == 2)
+                        temp[j][i] = array[j][i];
+                    if(count == 3)
+                        temp[j][i] = 1;
+                }
+                if (j == 24 && i>0 && i<99) {
+                    count = array[0][i+1] +
+                            array[0][i] +
+                            array[0][i-1] +
+                            array[j][i+1] +
+                            array[j-1][i+1] +
+                            array[j-1][i-1] +
+                            array[j][i-1] +
+                            array[j-1][i];
+                    if(count < 2 || count > 3)
+                        temp[j][i] = 0;
+                    if(count == 2)
+                        temp[j][i] = array[j][i];
+                    if(count == 3)
+                        temp[j][i] = 1;
+                }
+                if (j>0 && j<24 && i == 99) {
+                    count = array[j][0] +
+                            array[j+1][0] +
+                            array[j-1][0] +
+                            array[j][i-1] +
+                            array[j+1][i-1] +
+                            array[j-1][i-1] +
+                            array[j+1][i] +
+                            array[j-1][i];
+                    if(count < 2 || count > 3)
+                        temp[j][i] = 0;
+                    if(count == 2)
+                        temp[j][i] = array[j][i];
+                    if(count == 3)
+                        temp[j][i] = 1;
+                }
+                if (j == 0 && i == 0) {
+                    count = array[j][i+1] +
+                            array[j-1][i+1] +
+                            array[j-1][i] +
+                            array[j][99] +
+                            array[j+1][99] +
+                            array[24][i+1] +
+                            array[24][i] +
+                            array[24][99];
+                    if(count < 2 || count > 3)
+                        temp[j][i] = 0;
+                    if(count == 2)
+                        temp[j][i] = array[j][i];
+                    if(count == 3)
+                        temp[j][i] = 1;
+                }
+                if (j == 0 && i == 99) {
+                    count = array[j][i-1] +
+                            array[j-1][i-1] +
+                            array[j+1][i] +
+                            array[j][99] +
+                            array[j-1][99] +
+                            array[0][i-1] +
+                            array[0][i] +
+                            array[24][0];
+                    if(count < 2 || count > 3)
+                        temp[j][i] = 0;
+                    if(count == 2)
+                        temp[j][i] = array[j][i];
+                    if(count == 3)
+                        temp[j][i] = 1;
+                }
+                if (j == 24 && i == 99) {
+                    count = array[j][i-1] +
+                            array[j-1][i-1] +
+                            array[j-1][i] +
+                            array[j][0] +
+                            array[j-1][0] +
+                            array[99][i-1] +
+                            array[99][i] +
+                            array[0][0];
+                    if(count < 2 || count > 3)
+                        temp[j][i] = 0;
+                    if(count == 2)
+                        temp[j][i] = array[j][i];
+                    if(count == 3)
+                        temp[j][i] = 1;
+                }
+                if (j == 24 && i == 0) {
+                    count = array[j][i+1] +
+                            array[j-1][i+1] +
+                            array[j-1][i] +
+                            array[j][99] +
+                            array[j-1][99] +
+                            array[0][i+1] +
+                            array[0][i] +
+                            array[0][99];
+                    if(count < 2 || count > 3)
+                        temp[j][i] = 0;
+                    if(count == 2)
+                        temp[j][i] = array[j][i];
+                    if(count == 3)
+                        temp[j][i] = 1;
+                }
+            }
+        }
+    }
+    copy(temp, array);
+}
+
+*/
+
+
+/*
+
+#include <iostream>
+
+const int n=19, m=20, N=n*m; bool a[N], b[N];
+
+bool get(bool *a, int i, int j) { return a[(i+n)%n*n + (j+m)%m]; }
+
+int neibs(bool *a, int i, int j) {
+    int r = 0;
+    for(int p=-1; p<=1; p++) for(int q=-1; q<=1; q++) r += get(a, i+p, j+q);
+    return r - get(a, i, j);
+}
+void show(bool *a) {
+    for(int k=0; k<N; k++) std::cout<<(a[k] ? '@' : '-')<<((k+1)%n ? ' ' : '\n');}
+
+void go(bool *a, bool *b, int c) {
+    for(int i=0; i<=c; i++) {
+        std::cout << "generation " << i << ":\n"; show(a); std::cout<<'\n';
+        for(int k=0; k<N; k++) {
+            int s = neibs(a, k/n, k%n);
+            b[k] = !a[k] && s==3 ? 1 : a[k] && (s<2 || s>3) ? 0 : a[k];
+        }
+        bool *t=a; a=b; b=t;
+    }
+}
+int main() { for(int i=6; i<=13; i++) a[i*n+n/2]=1; go(a, b, 50); }
+
+*/
